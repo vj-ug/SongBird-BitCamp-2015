60b09d62f14d6a86c30fad4cb151113b63b35b82
vj-ug <vasujindal2015@gmail.com>
2015-08-01 10:41:40 -0500

    full

---
+++ twitter_streaming.py
@@ -0,0 +1,33 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token =  "85289745-4PknFj4zSUPd12rbIg8ZkPnAAewZCEmwXj3wyNbiO"
+access_token_secret =  "A0RNhwgoVh0okZQoL5w6UydpplyTSft1Sx6QCZ4TtvaAC"
+consumer_key =  "UIBl6otwQD9CtbhRQSQ2GlV8H:MlxVNNZDWfEDBpOTbZwOAPQ8BziP3tcQwMoU3vXdxllzsdgjLu"
+consumer_secret = "MlxVNNZDWfEDBpOTbZwOAPQ8BziP3tcQwMoU3vXdxllzsdgjLu"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['#NationalPetDay'])
